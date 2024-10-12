745f63e3a022bba93710d94466bb1f1b41b1811f
tim-hub <tim-hub@users.noreply.github.com>
2024-10-12 22:35:05 +1300

    update example and readme

--- README.md
+++ README.md
@@ -5,3 +5,31 @@
 
 The Prompt Smith JS SDK is a NodeJS package that provides a simple way to interact with the Prompt Smith API. 
 The SDK is designed to make it easy to integrate the Prompt Smith API into your LLM RAG and Agent applications, including support of Vercel AI / Edge functions.
+
+## Installation
+
+```bash
+
+npm install promptsmith-js-sdk
+
+```
+
+
+## Example
+
+```javascript
+import PromptSmith from "promptsmith-js-sdk/build/main/lib/sdk";
+const url =  'http://localhost:3000';
+const apiKey =  'api_key';
+
+const client = new PromptSmith(url, apiKey, 60);
+
+const uniqueKey = 'nz_tax_rate';
+
+client.getPrompt(uniqueKey).then((prompt) => {
+  console.log(prompt);
+}).catch((error) => {
+  console.error(error);
+});
+
+```
--- examples/ts-example.ts
+++ examples/ts-example.ts
@@ -1,8 +1,26 @@
+import PromptSmith from "promptsmith-js-sdk/build/main/lib/sdk";
+import dotenv from 'dotenv';
 
-import PromptSmith from "promptsmith-js-sdk";
+dotenv.config();
 
-
+/**
+ * This is an example of how to use the PromptSmith SDK in a TypeScript project.
+ */
 function main() {
-  const client = new PromptSmith('http://localhost:3000', 'api_key', 60);
-  console.log(client);
+
+  const url = process.env.PROMPTSMITH_URL || 'http://localhost:3000';
+  const apiKey = process.env.PROMPTSMITH_API_KEY || 'api_key';
+
+  const client = new PromptSmith(url, apiKey, 60);
+
+  const uniqueKey = 'nz_tax_rate';
+
+  client.getPrompt(uniqueKey).then((prompt) => {
+    console.log(prompt);
+  }).catch((error) => {
+    console.error(error);
+  });
 }
+
+
+main();
